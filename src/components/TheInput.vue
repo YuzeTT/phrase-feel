3a615d741e40ecb7c1cc14be9314dac03056a0c3
YuzeTT <ziv1024@163.com>
2023-03-26 21:30:59 +0800

    first commit

---
+++ src/components/TheInput.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+const { modelValue } = defineModel<{
+  modelValue: string
+}>()
+</script>
+
+<template>
+  <div border="~ rounded gray-200 dark:gray-700" p-1 flex gap-2>
+    <input
+      id="input"
+      v-model="modelValue"
+      flex-1
+      type="text"
+      v-bind="$attrs"
+      bg="transparent"
+      outline="none active:none"
+      px-2
+    >
+    <slot />
+  </div>
+</template>
---
+++ src/pages/index.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts" generic="T extends any, O extends any">
+defineOptions({
+  name: 'IndexPage',
+})
+
+const name = $ref('')
+
+const router = useRouter()
+const go = () => {
+  if (name)
+    router.push(`/hi/${encodeURIComponent(name)}`)
+}
+</script>
+
+<template>
+  <div>
+    <div i-ri-ancient-pavilion-fill text-4xl inline-block bg-indigo-500 />
+    <p>句情</p>
+    <p>
+      <em text-sm op75>在剧情中爽背古文</em>
+    </p>
+
+    <div py-4 />
+
+    <TheInput
+      v-model="name"
+      placeholder="我相信你不会用搜索框"
+      autocomplete="false"
+
+      @keydown.enter="go"
+    >
+      <button btn>
+        搜索
+      </button>
+    </TheInput>
+
+    <div>
+      <button
+        class="m-3 text-sm btn"
+        :disabled="!name"
+        @click="go"
+      >
+        Go
+      </button>
+    </div>
+  </div>
+</template>

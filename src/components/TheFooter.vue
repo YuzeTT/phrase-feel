4afc88758ff975a32bdc511d2552190e716104d1
YuzeTT <ziv1024@163.com>
2023-03-27 15:30:38 +0800

    feat: update footer

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -1,18 +1,25 @@
 <template>
   <div text-center>
     <div h-1px w-10 border="b base" my-4 mx-auto />
-    <nav text-xl mt-6 inline-flex gap-2>
-      <button icon-btn @click="toggleDark()">
-        <div dark:i-carbon-moon i-carbon-sun />
-      </button>
 
+    <nav inline-flex gap-3 mt-2>
+      <p>
+        <span op50>© 2023 </span>
+        <a
+          href="https://hsott.cn/" target="_blank"
+          title="YuzeTT's website" op50 hover:op80
+        >YuzeTT</a>
+      </p>
       <a
-        icon-btn i-carbon-logo-github
         rel="noreferrer"
-        href="https://github.com/antfu/vitesse-lite"
+        href="https://github.com/YuzeTT/phrase-feel"
         target="_blank"
         title="GitHub"
-      />
+        op50 hover:op80 flex gap-1 items-center transition
+      >
+        <div i-ri-github-fill />
+        Source Code
+      </a>
     </nav>
   </div>
 </template>
